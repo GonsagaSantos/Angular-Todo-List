5fa3451112bdb7304caf51ef4df21af14c29b5c8
Miguel Santos <miguellsantoss915@gmail.com>
2024-09-09 15:58:52 -0300

    .

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,17 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+//type User = {
+//  id: string;
+//  avatar: string;
+//  name: string;
+//}
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,16 +19,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() id!: string;
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required: true}) user!: User;
   @Output() select = new EventEmitter();
 
   get imagePath() {
-    return "assets/users/" + this.avatar;
+    return "assets/users/" + this.user.avatar;
   }
 
   onSelectedUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }

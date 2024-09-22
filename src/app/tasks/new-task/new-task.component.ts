9345ce5b1fe709469204685c0f5f8ff954efeea3
Miguel Santos <miguellsantoss915@gmail.com>
2024-09-22 19:57:33 -0300

    localStorage, services e injection

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type newTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,23 +11,27 @@ import { type newTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
   @Output() add = new EventEmitter<newTaskData>();
-
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
+  private tasksService = inject(TasksService);
 
   onCancel() {
-    this.cancel.emit()
+    this.close.emit()
   }
 
   onSubmit() {
-    this.add.emit({
-      title: this.enteredTitle,
-      summary: this.enteredSummary,
-      date: this.enteredDate
-    })
+    this.tasksService.addTask(
+      {
+        title: this.enteredTitle,
+        summary: this.enteredSummary,
+        date: this.enteredDate,
+      }, 
+      this.userId
+    );
+    this.close.emit();
   }
 }
